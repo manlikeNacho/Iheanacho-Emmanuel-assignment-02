9a24c0291e810152d4fdb1602371aaaa1c3344ef
Iheanacho Ebere <eiheanacho52@gmail.com>
2022-07-15 21:01:16 +0100

    assignment completed

--- src/assignment.js
+++ src/assignment.js
@@ -14,7 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   const value = 0;
+        const sum = arrayOfNumbers.reduce(
+            (a, b) => a + b,
+    value
+);
+    return sum;
 }
 // assignment.sumOfNumbers = sumOfNumbers;
 
@@ -28,7 +33,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+    // arrayOfNumbers.forEach(number => number%2===0 && (count += 1))
+    arrayOfNumbers = arrayOfNumbers.filter(element => element%2 === 0);
+    return count 
 }
 // assignment.countEvenNumbers = countEvenNumbers;
 
@@ -47,6 +55,8 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
+
+    arrayOfNumbers = arrayOfNumbers.map(element => Math.trunc((element * 9/5) + 32))
     return arrayOfNumbers;
 }
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
